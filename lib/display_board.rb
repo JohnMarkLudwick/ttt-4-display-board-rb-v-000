7f3d6690d37b25f25684d07a7045a893846aaa07
JohnMark <johnmarkludwick@gmail.com>
2018-05-09 10:57:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
 require 'pry'
 # out the current state.
+<<<<<<< HEAD
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -7,4 +8,9 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(board)
+  binding.pry
+>>>>>>> e4228f8bc773c8aef84a3a7bbc821965e81f47b6
 end
